19a7e9d64152dd36a6788dad225abc98f61a455c
kondratskivd <kondratskivd@gmail.com>
2021-04-09 16:56:50 +0300

    finished todo list

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,8 +1,14 @@
 export default function FilterButton(props) {
     return (
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
+        <button
+            key={props.name}
+            type="button"
+            className="btn toggle-btn"
+            aria-pressed={props.isPressed}
+            onClick={() => props.setFilter(props.name)}
+        >
             <span className="visually-hidden">Show </span>
-            <span>all</span>
+            <span>{props.name}</span>
             <span className="visually-hidden"> tasks</span>
         </button>
     )
